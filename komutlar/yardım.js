3b3d42f5fdbade74df9a67e2ad0c0cf8123a0324
hahahaezGG <43311837+hahahaezGG@users.noreply.github.com>
2018-10-03 18:39:11 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -11,7 +11,7 @@ exports.run = (client, message, params) => {
   .addField("**» Kullanıcı Komutları**", `-report = İstediğiniz Kullanıcıyı Reportlarsınız. \n-kısalt = İstediğiniz Linki Kısaltarak Özelleştire Bilirsiniz. \n-yaz = Bota İsediğinizi Yazdırırsınız. \n-sunucubilgi = Bulunduğunuz Sunucu Hakkında Bilgi Verir. \n-sunucuresmi = Bulunduğunuz Sunucunun Resmin Gösterir. \n-kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. \n-avatarım = Avatarınınızı Gösterir. `)
   .addField("**» Sunucu Yetkilisi Komutları**", `-ban = İstediğiniz Kişiyi Sunucudan Banlar. \n-kick  = İstediğiniz Kişiyi Sunucudan Atar. \n-unban = İstediğiniz Kişinin Yasağını Açar. \n-sustur = İstediğiniz Kişiyi Susturur. \n-sil = Belirtilen Miktarda Mesajı Silir. (MAX 100) \n-oylama = Oylama Açar. \n-duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
   .addField("**» Botun Ana Komutları**", "-yardım = BOT Komutlarını Atar. \n-yaz = BOT Yazi Yazar (Herkes Yapabilir). \n-ping = BOT Gecikme Süresini Söyler. \n-istatistik = BOT İstatistiklerini Gösterir.")
-  .setFooter('BlaxkLion Bot ')
+  .setFooter('SVO Bot ')
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
